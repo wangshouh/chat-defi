d4dfc17dc55661efd78dc01e9b1083decf93661d
Azleal <liyao.mr@gmail.com>
2025-05-17 17:58:15 +0800

    ai

---
+++ src/lib/dispatcher.ts
@@ -0,0 +1,43 @@
+import { generateObject } from "ai";
+
+import { z } from "zod";
+import { DEFAULT_MODEL } from "./openrouter";
+
+export const getDeFiProtocol = async (input: string) => {
+  const { object: actions } = await generateObject({
+    model: DEFAULT_MODEL,
+    schema: z.object({
+      actions: z
+        .enum([
+          "morpho::supply",
+          "morpho::withdraw",
+          "morpho::repay",
+          "morpho::falshloan",
+          "aave::supply",
+          "aave::withdraw",
+          "1inch::swap",
+        ])
+        .array(),
+    }),
+    prompt: `Evaluate the actions required to execute the transaction requested by the user and output them in order: ${input}`,
+  });
+
+  // actions.actions.map((action) => {
+  //   switch (action) {
+  //     case "morpho::repay": {
+  //       const { object: actions } = await generateObject({
+  //         model,
+  //         schema: z.object({
+
+  //         }),
+  //         prompt: `Evaluate the actions required to execute the transaction requested by the user and output them in order: ${input}`,
+  //       });
+  //     }
+  //   }
+  // });
+  return { actions };
+};
+
+// console.log(
+//   await getDeFiProtocol("我希望使用 ETH 向 Morpho 协议中的金库提供 USDC 资产")
+// );
